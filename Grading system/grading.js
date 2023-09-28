920184e1485e0f088f01f347c5d8cd4190649e53
shekinah <nyamburamary1212@gmail.com>
2023-09-28 11:56:10 +0300

    grading system

--- Grading system/grading.js
+++ Grading system/grading.js
@@ -1,17 +1,25 @@
-
+// The grade is displayed in the #grade paragraph element using the textContent property.
+let mark;
 function calculateGrade(mark) {
-    if (mark > 79) {            // if condition is met execute it.....
-      return 'A';
-    } else if (mark >= 60) {    //else if
-      return 'B';
-    } else if (mark >= 50) {
-      return 'C';
-    } else if (mark >= 40) {
-      return 'D';
+// Takes input values from user
+// If else statement
+    if (mark >= 80 && mark <= 100) {
+      return "A";
+    } else if (mark >= 60 && mark < 80) {
+      return "B";
+    } else if (mark >= 50 && mark < 60) {
+      return "C";
+    } else if (mark >= 40 && mark < 50) {
+      return "D";
+    } else if (mark >=0 && mark < 40) {
+      return "E";
     } else {
-      return 'E';      
+      return "Invalid Grade";
     }
   }
+
+
+
   // exucute multiple conditions
   module.exports = {
     calculateGrade,
